bac5ac318f1d341c845f4adeb84b3d0ec2cfb43c
Dir-A <g2576612205@gmail.com>
2023-03-27 10:57:09 +0800

    nop

---
+++ Minori_EN_MOD/Tools/Tools.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <Windows.h>
+
+BOOL WriteMemory(LPVOID lpAddress, LPCVOID lpBuffer, SIZE_T nSize);
+BOOL WriteHookCode(DWORD dwRawAddress, DWORD dwNewAddress, SIZE_T szHookCode);
+BOOL HookCreateFontA(DWORD dwCharSet, LPCSTR lpFontName);
